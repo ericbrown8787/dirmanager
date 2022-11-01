c84c4670299da32cff78db07ab05d7843fc771c7
Eric Brown <eric.brown8787@gmail.com>
2022-11-01 13:33:21 -0400

    Implement all options
    App now able to list directory contents, create a new directory and
    create a new file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 const { Command } = require('commander');
+const fs = require('fs');
+const path = require('path');
 const figlet = require('figlet');
 
 const program = new Command();
@@ -14,3 +16,47 @@ program
 const options = program.opts();
 
 console.log(figlet.textSync('Dir Manager'));
+
+async function listDirContents(filepath: string) {
+  try {
+    const files = await fs.promises.readdir(filepath);
+    const detailedFilesPromises = files.map(async (file: string) => {
+      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
+      const { size, birthtime } = fileDetails;
+      return { filename: file, 'size(KB)': size, created_at: birthtime };
+    });
+    const detailedFiles = await Promise.all(detailedFilesPromises);
+    console.table(detailedFiles);
+  } catch (error) {
+    console.error('Error occurred while reading the directory!', error);
+  }
+}
+
+function createDir(filepath: string) {
+  if (!fs.existsSync(filepath)) {
+    fs.mkdirSync(filepath);
+    console.log('The directory has been created successfully');
+  }
+}
+
+function createFile(filepath: string) {
+  fs.openSync(filepath, 'w');
+  console.log('An empty file has been created');
+}
+
+if (options.ls) {
+  const filepath = typeof options.ls === 'string' ? options.ls : __dirname;
+  listDirContents(filepath);
+}
+
+if (options.mkdir) {
+  createDir(path.resolve(__dirname, options.mkdir));
+}
+if (options.touch) {
+  createFile(path.resolve(__dirname, options.touch));
+}
+
+//Show the help page if no options have been passed
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
